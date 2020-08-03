34bd0787616bd923bf4cf9d826fc3c974a6602b1
apphow <apehow111@gmail.com>
2020-08-03 14:35:40 -0400

    added constant kTextFieldInputDecoration constant for city_screen to call, and a kButtonTextStyle for the FlatButton on the city_screen page

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/city_screen.dart';
 import 'package:weather_app/utilities/constants.dart';
 import 'package:weather_app/services/weather.dart';
 
@@ -77,7 +78,16 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
