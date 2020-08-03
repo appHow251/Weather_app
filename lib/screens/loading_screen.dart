881d98303da8c3d7ba21ae4e26d87ce6ff1203f2
apphow <apehow111@gmail.com>
2020-08-02 23:19:57 -0400

    Created location screen route setup

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:weather_app/services/networking.dart';
 
+import 'location_screen.dart';
+
 const apiKey = '81489127d7cd773b14c6b5fc40ef08dc';
 
 class LoadingScreen extends StatefulWidget {
@@ -29,6 +31,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
