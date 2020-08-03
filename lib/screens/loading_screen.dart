f4004ad870a45452400ba12e81160d3e9d9e9fbc
apphow <apehow111@gmail.com>
2020-08-02 21:30:05 -0400

    used opeweathermap api to get longitude and latitude

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,30 +3,35 @@ import 'package:weather_app/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+const apiKey = '81489127d7cd773b14c6b5fc40ef08dc';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
     getLocation();
-    print('triggering this line');
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
-    print(response.body);
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     // status code of 200 means everything's ok
     if (response.statusCode == 200) {
       String data = response.body;
